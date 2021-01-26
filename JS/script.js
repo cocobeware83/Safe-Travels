083449b5548c35f54d9eecc5d0e625a2ec914b24
cory neel <corycneel@gmail.com>
2021-01-26 17:52:44 -0600

    added 5 day forecast button and edited format for day of forecast

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ let savedSearchesDiv = document.getElementById("saved-searches");
 let searchButton = document.getElementById("btn");
 let searchBar = document.getElementById("searchbar");
 
+
 // check for local storage
 if(localStorage.getItem('city')===null){
     searchHistory = [];
@@ -115,15 +116,18 @@ function displayCityForecast(city){
   }).then(function(response){
       var weatherIcon = response.weather[0].icon;
       var date = $("<h2>").text(moment().format('l'));
-      var icon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+      var icon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + ".png");
       var tempFarenheit = (response.main.temp - 273.15) * 1.80 + 32;
+      var feelLike = (response.main.feels_like - 273.15) * 1.80 + 32;
+
           
       $("#city-name").text(response.name);
       $("#city-name").append(date);
       $("#city-name").append(icon);
       $("#city-temp").text(tempFarenheit.toFixed(2) + " \u00B0F");
       $("#city-humidity").text(response.main.humidity + "%");
-     
+      $("#city-feelslike").text(feelLike.toFixed(2) + " \u00B0F");
+
 
       var lat = response.coord.lat
       var lon = response.coord.lon
@@ -146,33 +150,7 @@ function displayCityForecast(city){
               $("#city-uvindex").text(response.value);});   
               $("#displayCity").show();}); 
           };
-          // begin api call for 5 day forecast
-          function fiveDayForecast(city){
-            var apiKey = "d6563c1f7289474849eef3ceaf635e1d"
-            var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
-        
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(response){
-                var counter = 1
-                for(var i=0; i < response.list.length; i += 8){
-                    var date = moment(response.list[i].dt_txt).format("l");
-                    var weatherIcon = response.list[i].weather[0].icon;
-                    var temperatureF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
-                        
-                    $("#day-" + counter).text(date);
-                    $("#icon" + counter).attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + ".png");
-                    $("#temp-" + counter).text(temperatureF.toFixed(2) + " \u00B0F");
-                    $("#humidity-" + counter).text(response.list[i].main.humidity + "%"); counter++;};
-                    $("#extended5").show();   
-                    });
-                    };
-        
-        function searchedCities(city){
-            var citiesListed = $("<li>").text(city)
-            citiesListed.addClass("searchedCity");
-            $("#searchedCity").append(citiesListed);};
+          
         
         //Clear input for new search
         function getCities(){
@@ -193,4 +171,6 @@ function displayCityForecast(city){
                 weather(cities[cities.length -1]);
             };};
         init();
+
+
         
